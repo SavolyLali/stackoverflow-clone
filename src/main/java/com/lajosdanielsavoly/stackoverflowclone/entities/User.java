4110650b5165e5b800944618cf1a529c0ea65478
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-02-26 19:12:46 +0100

    Create User class

---
+++ src/main/java/com/lajosdanielsavoly/stackoverflowclone/entities/User.java
@@ -0,0 +1,17 @@
+package com.lajosdanielsavoly.stackoverflowclone.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
