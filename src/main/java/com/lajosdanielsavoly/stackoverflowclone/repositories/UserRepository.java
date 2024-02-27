31db53302ccbaad6a7626d48ab9bc4a1678e48bb
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-02-27 10:21:48 +0100

    Create UserRepository interface

---
+++ src/main/java/com/lajosdanielsavoly/stackoverflowclone/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lajosdanielsavoly.stackoverflowclone.repositories;
+
+import com.lajosdanielsavoly.stackoverflowclone.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
