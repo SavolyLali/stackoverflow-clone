0907c751e4334f2b215f12eb1f69497210225992
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-02-27 13:41:50 +0100

    Create securityFilterChain

--- src/main/java/com/lajosdanielsavoly/stackoverflowclone/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/lajosdanielsavoly/stackoverflowclone/configuration/WebSecurityConfiguration.java
@@ -3,14 +3,36 @@ package com.lajosdanielsavoly.stackoverflowclone.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
+        return security
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/sign-up")
+                .permitAll()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
