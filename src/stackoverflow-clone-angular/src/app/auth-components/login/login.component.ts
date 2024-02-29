aa1375aa43db7d2de2806d1ea0c1ca0e6d61ec7d
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-02-29 17:43:50 +0100

    Log login information to console

--- src/stackoverflow-clone-angular/src/app/auth-components/login/login.component.ts
+++ src/stackoverflow-clone-angular/src/app/auth-components/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from "../../auth-services/auth-service/auth.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm!: FormGroup;
+
+  constructor(
+    private service: AuthService,
+    private fb: FormBuilder
+  ) {
+  }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+  }
 }
