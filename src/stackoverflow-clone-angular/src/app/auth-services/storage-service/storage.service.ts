d2487811368e3b0c9b0898991354ba60f3776db7
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-02-29 18:25:14 +0100

    Add saveUser function

--- src/stackoverflow-clone-angular/src/app/auth-services/storage-service/storage.service.ts
+++ src/stackoverflow-clone-angular/src/app/auth-services/storage-service/storage.service.ts
@@ -9,4 +9,10 @@ const USER = 'c_user';
 export class StorageService {
 
   constructor() { }
+
+  public saveUser(user: any) {
+    window.localStorage.removeItem(USER);
+    window.localStorage.setItem(USER, JSON.stringify(user));
+
+  }
 }
