5e4f47d48a069f65a2c888627bffbefbbb8fe993
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-02-29 14:49:29 +0100

    Validate if the password and confirm password is equal

--- src/stackoverflow-clone-angular/src/app/auth-components/signup/signup.component.ts
+++ src/stackoverflow-clone-angular/src/app/auth-components/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {AuthService} from "../../auth-services/auth-service/auth.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
@@ -14,7 +14,8 @@ export class SignupComponent {
   constructor(
     private service: AuthService,
     private fb: FormBuilder
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.signupForm = this.fb.group({
@@ -22,11 +23,22 @@ export class SignupComponent {
       email: ['', Validators.required],
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required]
-    });
+    }, {validator: this.confirmValidator});
+  }
+
+  private confirmValidator(fg: FormGroup) {
+    const password = fg.get('password')?.value;
+    const confirmPassword = fg.get('confirmPassword')?.value;
+    if (password !== confirmPassword) {
+      fg.get('confirmPassword')?.setErrors({passwordMismatch: true});
+    } else {
+      fg.get('confirmPassword')?.setErrors(null);
+    }
   }
 
 
   signup() {
     console.log(this.signupForm.value);
   }
+
 }
