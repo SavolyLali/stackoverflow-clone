f4fa469d53b673a7f87cea7d71cb1905ca051261
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-02-29 12:10:09 +0100

    Create login and signup components and add a navbar

--- src/stackoverflow-clone-angular/src/app/app.module.ts
+++ src/stackoverflow-clone-angular/src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { LoginComponent } from './auth-components/login/login.component';
+import { SignupComponent } from './auth-components/signup/signup.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
