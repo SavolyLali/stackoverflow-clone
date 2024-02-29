7db96735c9e707eb5dad46d69776983558d2d21f
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-02-29 18:55:18 +0100

    Store user's token and id in local storage

--- src/stackoverflow-clone-angular/src/app/auth-services/auth-service/auth.service.ts
+++ src/stackoverflow-clone-angular/src/app/auth-services/auth-service/auth.service.ts
@@ -1,21 +1,46 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from "@angular/common/http";
+import {map, Observable, tap} from "rxjs";
+import {StorageService} from "../storage-service/storage.service";
 
 
 const BASIC_URL = ['http://localhost:8080/']
+const AUTH_HEADER = 'Authorization';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private storage: StorageService
+  ) {
+  }
 
   signup(signupRequest: any): Observable<any> {
     return this.http.post(BASIC_URL + 'sign-up', signupRequest);
   }
 
   login(loginRequest: any): Observable<any> {
-    return this.http.post(BASIC_URL + 'authentication', loginRequest);
+    return this.http.post(BASIC_URL + 'authentication', loginRequest,
+      {observe: 'response'})
+      .pipe(
+        tap(__ => this.log("User Authentication")),
+        map((res: HttpResponse<any>) => {
+          this.storage.saveUser(res.body);
+          const authHeader = res.headers.get(AUTH_HEADER);
+          if (authHeader) {
+            const tokenLength = authHeader.length;
+            const bearerToken = authHeader.substring(7, tokenLength);
+            this.storage.saveToken(bearerToken);
+          }
+          return res;
+        })
+      );
+  }
+
+  log(message: string) {
+    console.log("User Auth Service" + message);
   }
 }
--- src/stackoverflow-clone-angular/src/app/auth-services/storage-service/storage.service.ts
+++ src/stackoverflow-clone-angular/src/app/auth-services/storage-service/storage.service.ts
@@ -13,6 +13,10 @@ export class StorageService {
   public saveUser(user: any) {
     window.localStorage.removeItem(USER);
     window.localStorage.setItem(USER, JSON.stringify(user));
+  }
 
+  public saveToken(token: string) {
+    window.localStorage.removeItem(TOKEN);
+    window.localStorage.setItem(TOKEN, token);
   }
 }
