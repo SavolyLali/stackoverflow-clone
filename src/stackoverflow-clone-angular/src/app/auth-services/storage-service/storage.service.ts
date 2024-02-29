dfc1961ea210378000d7b5969dde6288eb0d4443
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-02-29 18:17:53 +0100

    Create storage service

---
+++ src/stackoverflow-clone-angular/src/app/auth-services/storage-service/storage.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+const TOKEN = 'c_token';
+const USER = 'c_user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+}
