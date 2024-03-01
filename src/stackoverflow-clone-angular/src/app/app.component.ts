5ca5edf495d0adfbbaf90013a05261e871307631
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-01 11:00:01 +0100

    Update user's status if it is logged in or not

--- src/stackoverflow-clone-angular/src/app/app.component.ts
+++ src/stackoverflow-clone-angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {StorageService} from "./auth-services/storage-service/storage.service";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stackoverflow-clone-angular';
+
+  isUserLoggedIn!: boolean;
+
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  ngOnInit() {
+    this.updateUserLoginStatus();
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updateUserLoginStatus();
+      }
+    });
+  }
+
+  private updateUserLoginStatus() {
+    this.isUserLoggedIn = StorageService.isUserLoggedIn();
+  }
+
+  logout() {
+    StorageService.logout();
+    this.router.navigateByUrl('/login');
+  }
 }
