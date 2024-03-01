7dd3ffc09a3ab5cb3a7043a4ac084752b178dfc1
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-01 13:54:09 +0100

    Add UserGuard to dashboard path

--- src/stackoverflow-clone-angular/src/app/user/user-routing.module.ts
+++ src/stackoverflow-clone-angular/src/app/user/user-routing.module.ts
@@ -1,14 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {UserGuard} from "../auth-guards/user-guard/user.guard";
 
 const routes: Routes = [
-  {path: 'dashboard', component: DashboardComponent},
+    {path: 'dashboard', component: DashboardComponent, canActivate: [UserGuard]}
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 export class UserRoutingModule {
 }
