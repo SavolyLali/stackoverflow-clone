45bf056db7545fef337ee373f977a13d71b23d25
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-01 11:34:09 +0100

    Create QuestionDto

---
+++ src/main/java/com/lajosdanielsavoly/stackoverflowclone/dtos/QuestionDto.java
@@ -0,0 +1,16 @@
+package com.lajosdanielsavoly.stackoverflowclone.dtos;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class QuestionDto {
+
+    private Long id;
+    private String title;
+    private String body;
+    private List<String> tags;
+    private Long userId;
+
+}
