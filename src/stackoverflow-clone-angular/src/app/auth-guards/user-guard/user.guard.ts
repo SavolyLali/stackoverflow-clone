019b24f9e41a0f27d3354c473364567001922d2f
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-01 13:53:25 +0100

    Implement canActivate

--- src/stackoverflow-clone-angular/src/app/auth-guards/user-guard/user.guard.ts
+++ src/stackoverflow-clone-angular/src/app/auth-guards/user-guard/user.guard.ts
@@ -1,5 +1,33 @@
-import { CanActivateFn } from '@angular/router';
+import {Injectable} from "@angular/core";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
+import {StorageService} from "../../auth-services/storage-service/storage.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
-export const userGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable(
+    {
+        providedIn: 'root'
+    }
+)
+export class UserGuard implements CanActivate {
+
+    constructor(
+        private router: Router,
+        private snackBar: MatSnackBar
+    ) {
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot
+    ): boolean {
+        if (!StorageService.hasToken()) {
+            StorageService.logout();
+            this.router.navigateByUrl('/login');
+            this.snackBar.open(
+                'You are not logged-in. Log in first',
+                'Close',
+                {duration: 5000}
+            );
+            return false;
+        }
+        return true;
+    }
+}
