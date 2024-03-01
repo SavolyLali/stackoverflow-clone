63d68b7d95e33c0af84ddb4b20aa242f0561cfdc
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-01 11:27:09 +0100

    Create Questions entity

---
+++ src/stackoverflow-clone-angular/.angular/cache/17.2.2/vite/deps/@angular_material_button.js
@@ -0,0 +1,33 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-4CDZBC6B.js";
+import "./chunk-TQBYA56P.js";
+import "./chunk-6LEEHN4L.js";
+import "./chunk-6S4F5JB6.js";
+import "./chunk-SXIXOCJ4.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
