41b3a211b782a506b70796fcbb843aeb2112069f
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-02 12:12:38 +0100

    Add postQuestion method

--- src/stackoverflow-clone-angular/src/app/user/user-services/question-service/question.service.ts
+++ src/stackoverflow-clone-angular/src/app/user/user-services/question-service/question.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 const BASIC_URL = ['http://localhost:8080/'];
 
@@ -10,5 +11,10 @@ export class QuestionService {
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) {
+  }
+
+  postQuestion(questionDto: any): Observable<any> {
+    return this.http.post<[]>(BASIC_URL + 'api/question', questionDto);
+  }
 }
