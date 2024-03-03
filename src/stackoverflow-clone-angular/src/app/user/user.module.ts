60247c2f967d00c5b82acaee7e7f5e70c034e44f
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-03 11:44:20 +0100

    Update imported modules

--- src/stackoverflow-clone-angular/src/app/user/user.module.ts
+++ src/stackoverflow-clone-angular/src/app/user/user.module.ts
@@ -5,8 +5,6 @@ import {ReactiveFormsModule, FormsModule} from "@angular/forms";
 import {UserRoutingModule} from './user-routing.module';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {PostQuestionComponent} from './components/post-question/post-question.component';
-import {MatError, MatFormField} from "@angular/material/form-field";
-import {MatButton} from "@angular/material/button";
 
 ///////////////////////
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
@@ -14,10 +12,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatIconModule} from '@angular/material/icon';
-
-
-
-
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
@@ -31,16 +26,14 @@ import {MatIconModule} from '@angular/material/icon';
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
-    MatFormField,
-    MatError,
-    MatButton,
 
     ///////
     MatProgressSpinnerModule,
     MatInputModule,
     MatFormFieldModule,
     MatChipsModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class UserModule {
