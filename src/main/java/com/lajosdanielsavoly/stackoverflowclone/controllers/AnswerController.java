1fdb8efca590be79c925a64f983ce4a96e614f95
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-10 14:46:24 +0100

    Create AnswerController

---
+++ src/main/java/com/lajosdanielsavoly/stackoverflowclone/controllers/AnswerController.java
@@ -0,0 +1,9 @@
+package com.lajosdanielsavoly.stackoverflowclone.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/answer")
+public class AnswerController {
+}
