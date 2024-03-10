b2f04dc2acf89ea541d26b6a754666195b3395f6
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-10 14:55:19 +0100

    Create postAnswer method

--- src/main/java/com/lajosdanielsavoly/stackoverflowclone/services/answer/AnswerService.java
+++ src/main/java/com/lajosdanielsavoly/stackoverflowclone/services/answer/AnswerService.java
@@ -1,4 +1,7 @@
 package com.lajosdanielsavoly.stackoverflowclone.services.answer;
 
+import com.lajosdanielsavoly.stackoverflowclone.dtos.AnswerDto;
+
 public interface AnswerService {
+    AnswerDto postAnswer(AnswerDto answerDto);
 }
