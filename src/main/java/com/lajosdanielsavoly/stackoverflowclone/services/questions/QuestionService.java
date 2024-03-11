3a25859dbf47f62fd8055bb01451f18404615716
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-11 14:30:30 +0100

    Modify method return type to SingleQuestionDto

--- src/main/java/com/lajosdanielsavoly/stackoverflowclone/services/questions/QuestionService.java
+++ src/main/java/com/lajosdanielsavoly/stackoverflowclone/services/questions/QuestionService.java
@@ -3,11 +3,12 @@ package com.lajosdanielsavoly.stackoverflowclone.services.questions;
 
 import com.lajosdanielsavoly.stackoverflowclone.dtos.AllQuestionResponseDto;
 import com.lajosdanielsavoly.stackoverflowclone.dtos.QuestionDto;
+import com.lajosdanielsavoly.stackoverflowclone.dtos.SingleQuestionDto;
 
 public interface QuestionService {
     QuestionDto addQuestion(QuestionDto questionDto);
 
     AllQuestionResponseDto getAllQuestions(int pageNumber);
 
-    QuestionDto getQuestionById(Long questionId);
+    SingleQuestionDto getQuestionById(Long questionId);
 }
