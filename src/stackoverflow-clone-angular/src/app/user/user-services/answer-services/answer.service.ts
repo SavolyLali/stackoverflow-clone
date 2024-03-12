927a2d2480d96fd832e2f2dd1414af067adb0111
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-12 10:31:18 +0100

    Create answer service

---
+++ src/stackoverflow-clone-angular/src/app/user/user-services/answer-services/answer.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+
+const BASIC_URL = ['http://localhost:8080/'];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnswerService {
+
+  constructor() {
+  }
+}
