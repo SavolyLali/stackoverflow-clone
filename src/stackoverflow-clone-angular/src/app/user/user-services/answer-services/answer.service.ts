1279008ae9a64756f4eccfee61dc5f4c8ea1e1ab
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-12 10:32:33 +0100

    Add createAuthorizationHeader method

--- src/stackoverflow-clone-angular/src/app/user/user-services/answer-services/answer.service.ts
+++ src/stackoverflow-clone-angular/src/app/user/user-services/answer-services/answer.service.ts
@@ -1,4 +1,6 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {StorageService} from "../../../auth-services/storage-service/storage.service";
 
 const BASIC_URL = ['http://localhost:8080/'];
 
@@ -7,6 +9,13 @@ const BASIC_URL = ['http://localhost:8080/'];
 })
 export class AnswerService {
 
-  constructor() {
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  createAuthorizationHeader(): HttpHeaders {
+    let authHeaders = new HttpHeaders();
+    return authHeaders.set('Authorization', 'Bearer ' + StorageService.getToken());
   }
 }
