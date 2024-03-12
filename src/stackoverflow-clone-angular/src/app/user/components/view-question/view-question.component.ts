e83caa9f223e2ead530f542e839e88bda717264a
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-12 16:37:31 +0100

    Inform user if the asnwer is posted or notgit status

--- src/stackoverflow-clone-angular/src/app/user/components/view-question/view-question.component.ts
+++ src/stackoverflow-clone-angular/src/app/user/components/view-question/view-question.component.ts
@@ -4,6 +4,7 @@ import {ActivatedRoute} from "@angular/router";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {StorageService} from "../../../auth-services/storage-service/storage.service";
 import {AnswerService} from "../../user-services/answer-services/answer.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-view-question',
@@ -20,7 +21,8 @@ export class ViewQuestionComponent {
     private questionService: QuestionService,
     private answerService: AnswerService,
     private activatedRoute: ActivatedRoute,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
   ) {
   }
 
@@ -49,6 +51,15 @@ export class ViewQuestionComponent {
     this.answerService.postAnswer(data).subscribe(
       (response) => {
         console.log(response);
+        if (response.id !== null) {
+          this.snackBar.open('Answer posted successfully', 'Close', {
+            duration: 5000,
+          });
+        } else {
+          this.snackBar.open('Something went wrong', 'Close', {
+            duration: 5000,
+          });
+        }
       }
     );
   }
