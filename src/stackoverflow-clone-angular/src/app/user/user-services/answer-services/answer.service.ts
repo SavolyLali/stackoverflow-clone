60a8315f9bb82667bbc48b94472bef03b8c3c314
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-12 10:34:21 +0100

    Add postAnswer method

--- src/stackoverflow-clone-angular/src/app/user/user-services/answer-services/answer.service.ts
+++ src/stackoverflow-clone-angular/src/app/user/user-services/answer-services/answer.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {StorageService} from "../../../auth-services/storage-service/storage.service";
+import {Observable} from "rxjs";
 
 const BASIC_URL = ['http://localhost:8080/'];
 
@@ -12,6 +13,13 @@ export class AnswerService {
   constructor(
     private http: HttpClient
   ) {
+  }
+
+  postAnswer(answerDto: any): Observable<any> {
+    answerDto.userId = StorageService.getUserId();
+    console.log(answerDto);
+    return this.http.post<[]>(BASIC_URL + 'api/answer', answerDto,
+      {headers: this.createAuthorizationHeader()});
   }
 
   createAuthorizationHeader(): HttpHeaders {
