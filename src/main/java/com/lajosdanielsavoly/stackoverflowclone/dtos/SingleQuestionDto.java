24fbfd16c84e78a1964b0e5ee206f6c65f8185db
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2024-03-16 13:48:34 +0100

    Add answerDtoList field

--- src/main/java/com/lajosdanielsavoly/stackoverflowclone/dtos/SingleQuestionDto.java
+++ src/main/java/com/lajosdanielsavoly/stackoverflowclone/dtos/SingleQuestionDto.java
@@ -2,8 +2,11 @@ package com.lajosdanielsavoly.stackoverflowclone.dtos;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class SingleQuestionDto {
 
     private QuestionDto questionDto;
+    private List<AnswerDto> answerDtoList;
 }
